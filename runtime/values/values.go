45c8dfc1999c509b5d62bf015f8096dd77359348
ber2minsin <ber2minsin@gmail.com>
2024-11-25 15:41:23 -0500

    Added: Basic values

---
+++ runtime/values/values.go
@@ -0,0 +1,26 @@
+package values
+
+type ValueType string
+
+const (
+	NoneValue   ValueType = "None"
+	NumberValue ValueType = "Number"
+)
+
+type RtVal interface {
+	GetType()
+}
+
+type NumVal struct {
+	Type  ValueType `json:"type"`
+	Value float64   `json:"value"`
+}
+
+func (nv *NumVal) GetType() ValueType { return nv.Type }
+
+type NoneVal struct {
+	Type  ValueType
+	Value string
+}
+
+func (nov *NoneVal) GetType() ValueType { return nov.Type }
